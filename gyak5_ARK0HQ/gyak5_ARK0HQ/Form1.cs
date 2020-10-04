d04de4fb0f234a8bebe3dee29c7ba3b0e653e2d4
Réka Kovács <reka.kovacs8@stud.uni-corvinus.hu>
2020-10-04 22:10:48 +0200

    commit5

--- gyak5_ARK0HQ/gyak5_ARK0HQ/Form1.cs
+++ gyak5_ARK0HQ/gyak5_ARK0HQ/Form1.cs
@@ -27,13 +27,51 @@ namespace gyak5_ARK0HQ
             InitializeComponent();
             LoadData();
 
-           
+            try
+            {
+               
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
             
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                   "Kód",
+                   "Eladó",
+                   "Oldal",
+                   "Kerület",
+                   "Lift",
+                   "Szobák száma",
+                   "Alapterület (m2)",
+                   "Ár (mFt)",
+                   "Négyzetméter ár (Ft/m2)"};
         }
     }
 }
