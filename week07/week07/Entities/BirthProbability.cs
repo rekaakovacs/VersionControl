4769f068b5f511195f18f23978d6d40eb5c6b545
rekaakovacs <reka.kovacs8@stud.uni-corvinus.hu>
2020-10-25 21:10:40 +0100

    commit4

---
+++ week07/week07/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class BirthProbability
+    {
+        public double Age { get; set; }
+        public double NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
