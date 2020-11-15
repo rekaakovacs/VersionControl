5af6ec98e42316819572670bbbe86e293aee176d
rekaakovacs <reka.kovacs8@stud.uni-corvinus.hu>
2020-11-15 14:30:29 +0100

    commit6

--- week08/week08/Entities/Ball.cs
+++ week08/week08/Entities/Ball.cs
@@ -5,37 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Abstractions;
 
 namespace week08.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(GetGraphics(e));
-        }
-
-        private static Graphics GetGraphics(PaintEventArgs e)
-        {
-            return e.Graphics;
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
     }
 }
